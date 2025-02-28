c2b83599dd89624b0d3ca4e5f48a63900fae3f1b
Ivan Radmilovic <radmilovicivan@gmail.com>
2025-02-28 11:45:47 +0100

    initialize global state instruction

--- programs/sol-strike/src/lib.rs
+++ programs/sol-strike/src/lib.rs
@@ -11,6 +11,13 @@ const CHIP_PRICE :u64 = (LAMPORTS_PER_SOL as f64 * 0.01) as u64;
 pub mod sol_strike {
     use super::*;
 
+    pub fn initalize_global_config(ctx: Context<InitializeGlobalConfig>, lamport_price: u64) -> Result<()> {
+        let global_config_state = &mut ctx.accounts.initalize_global_config;
+        global_config_state.lamport_price = lamport_price;
+        global_config_state.bump = ctx.bumps.initalize_global_config;
+        Ok(())
+    }
+
     pub fn add_token(ctx: Context<AddToken>, token_address: Pubkey, token_price: u64) -> Result<()> {
         let chip_token_price_state = &mut ctx.accounts.chip_token_price_state;
         chip_token_price_state.token_address = token_address;
@@ -37,8 +44,10 @@ pub mod sol_strike {
 }
 
 #[account]
+#[derive(InitSpace)]
 pub struct GlobalConfigState {
     pub lamport_price: u64,
+    pub bump: u8
 }
 
 #[account]
@@ -49,6 +58,23 @@ pub struct ChipTokenPriceState {
     pub bump: u8,
 }
 
+#[derive(Accounts)]
+pub struct InitializeGlobalConfig<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = ANCHOR_DISCRIMINATOR + GlobalConfigState::INIT_SPACE,
+        seeds = [b"GLOBAL_CONFIG_STATE"],
+        bump
+    )]
+    pub initalize_global_config: Account<'info, GlobalConfigState>,
+    #[account(
+        mut
+    )]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
 #[derive(Accounts)] 
 #[instruction(token_address: Pubkey)]
 pub struct AddToken<'info> {
@@ -56,7 +82,7 @@ pub struct AddToken<'info> {
         init,
         payer = signer,
         space = ANCHOR_DISCRIMINATOR + ChipTokenPriceState::INIT_SPACE,
-        seeds = [b"chip_token_price", token_address.key().as_ref()],
+        seeds = [b"CHIP_TOKEN_PRICE", token_address.key().as_ref()],
         bump
     )]
     pub chip_token_price_state: Account<'info, ChipTokenPriceState>,
