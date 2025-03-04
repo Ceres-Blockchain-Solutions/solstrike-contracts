eac366936fa695c04b8819a04d0ebb744a85853c
Ivan Radmilovic <radmilovicivan@gmail.com>
2025-03-04 17:50:37 +0100

    test: Is initialized

--- tests/sol-strike.ts
+++ tests/sol-strike.ts
@@ -1,16 +1,36 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolStrike } from "../target/types/sol_strike";
+import { assert } from "chai";
 
 describe("sol-strike", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.getProvider();
 
   const program = anchor.workspace.SolStrike as Program<SolStrike>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
+    const [chipMintPDA] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("CHIP_MINT")],
+      program.programId
+    );
+    
+    const [treasuryPDA] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("TREASURY")],
+      program.programId
+    );
+
     const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    console.log("Your transaction signature:", tx);
+
+    const chipMintAccount = await provider.connection.getAccountInfo(chipMintPDA);
+    const treasuryAccount = await provider.connection.getAccountInfo(treasuryPDA);
+
+    console.log("Chip Mint PDA:", chipMintPDA.toBase58());
+    console.log("Treasury PDA:", treasuryPDA.toBase58());
+
+    assert(chipMintAccount !== null, "chip_mint PDA was not created!");
+    assert(treasuryAccount !== null, "treasury PDA was not created!");
   });
 });
